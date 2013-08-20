f30556eda3892cfe7208fc37695f0880243208c6
Torleiv Flatebo <tor@flatebo.org>
2013-08-20 11:37:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Parse Anita's website and pull out the daily special. I am sure this will break at some point.
---
+++ web.rb
@@ -0,0 +1,82 @@
+require 'nokogiri'
+require 'open-uri'
+require 'sinatra'
+require 'haml'
+require 'date'
+
+class LunchSpecial
+  attr_accessor :title, :url, :content
+
+  def initialize
+    @title = "Anita's Daily Lunch Special"
+    @url = 'http://www.truetastes.com/anitas-cafe-lunch-catering/'
+    @content = 'whipped potatoes'
+  end
+
+end
+
+get '/', :provides => 'html' do
+  @lunch_special = get_anitas
+  haml :index
+end
+
+get '/atom', :provides => ['rss', 'atom', 'xml'] do
+  @lunch_special = LunchSpecial.new
+  @lunch_special.content = get_anitas
+
+  builder = Nokogiri::XML::Builder.new do |xml|
+    xml.feed('xmlns' => 'http://www.w3.org/2005/Atom') {
+      xml.link('href' => @lunch_special.url)
+      xml.title @lunch_special.title
+      xml.updated get_updated(@lunch_special.content)
+      xml.entry {
+        xml.title @lunch_special.title
+        xml.content @lunch_special.content
+        xml.id get_updated(@lunch_special.content)
+        xml.updated get_updated(@lunch_special.content)
+        xml.link('href' => @lunch_special.url)
+      }
+    }
+  end
+
+  return builder.to_xml
+
+end
+
+get '/anitas' do
+  return get_anitas
+end
+
+def get_anitas
+
+  lunch_special = "Whipped potatoes"
+
+  doc = Nokogiri::HTML(open('http://www.truetastes.com/anitas-cafe-lunch-catering/'))
+
+  ####
+  # Search for nodes by css
+
+  doc.css('div.specials_copy').each do |specials|
+    specials.xpath('.//p').each do |container|
+      inner_html = container.inner_html
+      # pull out the text up to the first HTML tag
+      lunch_special = inner_html[/[^\<]+/].strip!
+    end
+  end
+
+  return lunch_special
+
+end
+
+# the input to this method is a bit wack
+# e.g. "Tuesday August 20th ENTREE:  Croque Monsieur with Pasta Salad"
+def get_updated(in_str)
+
+  matches = in_str.match(/^(.*)\sENTREE:/)
+
+  in_dt = DateTime.parse(matches[1])
+
+  return in_dt.to_s
+
+end
+
